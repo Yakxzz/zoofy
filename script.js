7120cea3d1b29879cedc57837bddb80255bcffbe
Yakxzz <animatingstudeo@gmail.com>
2026-02-15 22:47:36 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -1045,9 +1045,3 @@ function showNotification(message) {
         setTimeout(() => notification.remove(), 400);
     }, 2500);
 }
-<!-- start webpushr tracking code --> 
-<script>(function(w,d, s, id) {if(typeof(w.webpushr)!=='undefined') return;w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};var js, fjs = d.getElementsByTagName(s)[0];js = d.createElement(s); js.id = id;js.async=1;js.src = "https://cdn.webpushr.com/app.min.js";
-fjs.parentNode.appendChild(js);}(window,document, 'script', 'webpushr-jssdk'));
-webpushr('setup',{'key':'BO78IH7GO_Hge5PBnXPhBM8rFvlF7BBZTH5sgehkfFky--_o_M5jnMF5xbyCCd8kMmwvw6jdDuvP4KzmM_DEmvo' });</script>
-<!-- end webpushr tracking code -->
-
